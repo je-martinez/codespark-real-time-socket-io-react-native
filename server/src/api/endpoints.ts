58e2e7d06da1e36c04419e51b74d47c58be5d2b9
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-11-25 20:51:10 -0600

    feat: (api) create endpoint for reset

---
+++ server/src/api/endpoints.ts
@@ -0,0 +1,31 @@
+import type { Express, Request, Response } from "express";
+import { rooms } from "../models/rooms";
+import { users } from "../models/users";
+
+export const registerEndpoints = (app: Express) => {
+  app.delete("/api/reset", (req: Request, res: Response) => {
+    const apiKey = req.headers["x-api-key"];
+
+    if (apiKey !== process.env.API_KEY) {
+      res.status(401).send({
+        message: "Unauthorized",
+      });
+      return;
+    }
+
+    console.log("Resetting rooms");
+    rooms.forEach((room) => {
+      room.messages = [];
+      room.users = [];
+    });
+
+    console.log("Resetting users");
+    users.clear();
+    console.log("Reset complete");
+
+    res.status(200).send({
+      message: "Reset complete",
+    });
+    return;
+  });
+};
--- server/src/api/server.ts
+++ server/src/api/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import http from "http";
 import { setupSocketIo } from "./io";
+import { registerEndpoints } from "./endpoints";
 
 export const app = express();
 export const server = http.createServer(app);
 export const ioServer = setupSocketIo(server);
+registerEndpoints(app);
